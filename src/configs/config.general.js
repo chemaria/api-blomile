9a6837a531eb5c7db84548dac8f34980902b302e
unknown <josecruzigle@gmail.com>
2022-03-09 23:47:04 +0100

    borra var entorno

--- src/configs/config.general.js
+++ src/configs/config.general.js
@@ -2,7 +2,5 @@ import 'dotenv/config.js'
 
 const env = process.env
 export const generalConfig = {
-  jwt_secret: env.JWT_SECRET_KEY,
-  jwt_cookie_expires: env.JWT_COOKIE_EXPIRES,
-  jwt_time_live: env.JWT_TIME_TO_LIVE
+  jwt_secret: env.JWT_SECRET_KEY
 }
