742182a6727fa66997eed4a828b01ada74aa0e6c
unknown <josecruzigle@gmail.com>
2022-03-09 10:26:29 +0100

    cors y cambios en login

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ import bodyParser from 'body-parser'
 import express from 'express'
 import { userRouter } from './src/routes/userRouter/router.js'
 import { postRouter } from './src/routes/postRouter/router.js'
+import cors from 'cors'
 
 const app = express()
 const port = 3002
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
+}))
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
--- src/controllers/userController/login.controller.js
+++ src/controllers/userController/login.controller.js
@@ -1,8 +1,38 @@
 import connectDb from '../../services/db.connect.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { generalConfig } from '../../configs/config.general.js'
+import cookie from 'cookie'
 
 export default async function userLogin (req, res, next) {
+  console.log('pasa por aqui')
   const { user, password } = req.body
-  const [result] = await connectDb({ sql: 'SELECT id,password FROM users WHERE username = ?', params: user })
-  console.log(result)
+  if (!(user && password)) { return res.status(401).json('error invalid user or password') }
+
+  const [result] = await connectDb({
+    sql: 'SELECT id,password FROM users WHERE username = ?',
+    params: user
+  })
+  if (!result) { return res.status(401).json('error invalid user or password') }
+
+  const passCompare = await bcrypt.compare(password, result.password)
+  if (!passCompare) { return res.status(401).json('error invalid user or password') }
+
+  const jwtData = {
+    userData: {
+      username: user,
+      id: result.id
+    },
+    tokenExpires: { expiresIn: '30d' },
+    cookieData: {
+      maxAge: 60 * 60 * 24 * 7
+    }
+  }
+  const token = await jwt.sign(jwtData.userData, generalConfig.jwt_secret, jwtData.tokenExpires)
+
+  res.setHeader('Set-Cookie', cookie.serialize('jwt', token, jwtData.cookieData)).status(201)
+  res.status(201)
+  res.end()
+  const cookie1 = cookie.jwt
+  console.log('este es el token' + token + ' esta es la cookie ' + cookie1)
 }
